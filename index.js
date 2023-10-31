4f5854a073b67b897b3555cc6105a1e00d764e8e
AleksDimitrov <adimitrov500@gmail.com>
2023-10-30 17:39:51 -0700

    Improving Privacy
    Editing Footer and Securing Contact Info

--- index.js
+++ index.js
@@ -11,22 +11,34 @@ function showDivs(n) {
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length} ;
     for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     x[slideIndex-1].style.display = "block";
+
+    var y = document.getElementsByClassName("captions");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    y[slideIndex-1].style.display = "block";
 }
 
 function showDivs2(n) {
     var i;
     var x = document.getElementsByClassName("slides");
     for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     x[n-1].style.display = "block";
+
+    var y = document.getElementsByClassName("captions");
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = "none";
+    }
+    y[n-1].style.display = "block";
 }
 
 function openResume() {
-    window.open("./AleksanderD_ResumeFullV8.pdf");
+    window.open("./AleksanderD_Resume.pdf");
 }
 
 function openLinkedIn() {
@@ -35,4 +47,9 @@ function openLinkedIn() {
 
 function openGitHub() {
     window.open("https://github.com/AleksDimitrov");
+}
+
+function goToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
